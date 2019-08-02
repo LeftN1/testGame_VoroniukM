9f1c17c1fd1eca8e34b880d0256571f150f1224a
LeftN1 <voroniukm@ukr.net>
2019-08-03 01:38:33 +0300

    drag n drop

--- core/src/com/voroniuk/testgame/FGame.java
+++ core/src/com/voroniuk/testgame/FGame.java
@@ -2,11 +2,23 @@ package com.voroniuk.testgame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Payload;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Source;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.voroniuk.testgame.models.Fruit;
 import com.voroniuk.testgame.models.Harvest;
@@ -19,6 +31,7 @@ public class FGame extends ApplicationAdapter {
 	OrthographicCamera camera;
 	ExtendViewport viewport;
 	Sprite backGround;
+	Stage stage;
 
 
 	Harvest h;
@@ -27,47 +40,106 @@ public class FGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 1143, 1386);
 
-		textureAtlas = new TextureAtlas("sprites.txt");
-		batch = new SpriteBatch();
+		viewport = new ExtendViewport(800, 600, camera);
+		stage = new Stage();
+		stage.setViewport(viewport);
 
-		backGround = textureAtlas.createSprite("background");
-		scaleSprite(backGround, SCALE);
 
-		h = Fruit.getFirst();
+		Gdx.input.setInputProcessor(stage);
 
-		item = textureAtlas.createSprite(h.getSpriteName());
-		scaleSprite(item, SCALE);
+		final Skin skin = new Skin();
+		skin.add("default", new LabelStyle(new BitmapFont(), Color.WHITE));
+		skin.add("background", new Texture("background.png"));
+		skin.add("apple", new Texture("apple.png"));
+		skin.add("carrot", new Texture("carrot.png"));
 
+
+//		camera = new OrthographicCamera();
+//		camera.setToOrtho(false, 1143, 1386);
+//
+		final Image sourceImage = new Image(skin, "apple");
+		sourceImage.setBounds(0, 0, 112, 102);
+		stage.addActor(sourceImage);
+
+		Image validTargetImage = new Image(skin, "apple");
+		validTargetImage.setBounds(300, 300, 112, 102);
+		stage.addActor(validTargetImage);
+
+		Image invalidTargetImage = new Image(skin, "carrot");
+		invalidTargetImage.setBounds(500, 500, 112, 102);
+		stage.addActor(invalidTargetImage);
+
+		DragAndDrop dragAndDrop = new DragAndDrop();
+
+		dragAndDrop.addSource(new Source(sourceImage) {
+			public Payload dragStart (InputEvent event, float x, float y, int pointer) {
+				Payload payload = new Payload();
+				payload.setObject(sourceImage);
+
+				payload.setDragActor(new Label("Some payload!", skin));
+
+				Label validLabel = new Label("Some payload!", skin);
+				validLabel.setColor(0, 1, 0, 1);
+				payload.setValidDragActor(validLabel);
+
+				Label invalidLabel = new Label("Some payload!", skin);
+				invalidLabel.setColor(1, 0, 0, 1);
+				payload.setInvalidDragActor(invalidLabel);
+
+				return payload;
+			}
+		});
+
+		dragAndDrop.addTarget(new DragAndDrop.Target(validTargetImage) {
+			public boolean drag (Source source, Payload payload, float x, float y, int pointer) {
+				getActor().setColor(Color.GREEN);
+				return true;
+			}
+
+			public void reset (Source source, Payload payload) {
+				getActor().setColor(Color.WHITE);
+			}
+
+			public void drop (Source source, Payload payload, float x, float y, int pointer) {
+				System.out.println("Accepted: " + payload.getObject() + " " + x + ", " + y);
+			}
+		});
+		dragAndDrop.addTarget(new DragAndDrop.Target(invalidTargetImage) {
+			public boolean drag (Source source, Payload payload, float x, float y, int pointer) {
+				getActor().setColor(Color.RED);
+				return false;
+			}
+
+			public void reset (Source source, Payload payload) {
+				getActor().setColor(Color.WHITE);
+			}
+
+			public void drop (Source source, Payload payload, float x, float y, int pointer) {
+			}
+		});
 
 	}
 
 	@Override
 	public void render () {
-//		Gdx.gl.glClearColor(0.57f, 0.77f, 0.85f, 1);
-//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		camera.update();
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		backGround.draw(batch);
-		item.draw(batch);
-		batch.end();
+
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
 	}
-	
+
 	@Override
 	public void dispose () {
-		textureAtlas.dispose();
-		batch.dispose();
+		stage.dispose();
 
 	}
 
 	@Override
 	public void resize(int width, int height) {
 
-		camera.setToOrtho(false, width, height);
-//		camera.update();
-		batch.setProjectionMatrix(camera.combined);
+		stage.getViewport().update(width, height, true);
+
 	}
 
 	public void scaleSprite(Sprite sprite, float scale){
